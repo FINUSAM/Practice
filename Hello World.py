2ffdd3880d43bb2efe8c923f9d8c06f60752386b
FINUSAM <faheemsameer10@gmail.com>
2019-05-06 19:26:39 +0300

    Merge branch 'master' of https://github.com/FINUSAM/Practice
    # Conflicts:
    #	README.md

--- Hello World.py
+++ Hello World.py
@@ -1,12 +1,12 @@
 # Real Work Starts Here
 # GONNA MAKE AN ERROR
-#this helps with the error to happen
+# this helps with the error to happen
 
-def  main(msg):
+def main(msg):
     print(msg)
 
 
 main("HELLO PEOPLE")
 
-#create a simple change for
-#creating a merge conflict
+# create a simple change for
+# creating a merge conflict
